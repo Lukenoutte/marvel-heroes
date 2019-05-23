fb10b82d6eaf377ca295f6c989d13f095c16cecc
Lukenoutte <lucas.alima@souunit.com.br>
2019-05-23 17:04:09 -0300

    Load new heroes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:convert/convert.dart';
+import 'package:crypto/crypto.dart' as crypto;
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:programacao_mobile/heroesdetail.dart';
 import 'package:programacao_mobile/Heroes.dart';
+import 'package:dio/dio.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -29,11 +33,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   var publicKey ="22ec9cca057f5d000d1d4e535b743e61";
   var privateKey = "979814d8b2e2dbd1a91415ff2f7223686af95b3e";
-
-  var url = "https://gateway.marvel.com:443/v1/public/characters?ts=1&orderBy=name&apikey=22ec9cca057f5d000d1d4e535b743e61&hash=7ba7945cb850a0673bbb1e5633fd0ca7";
+  var characters = new List<Heroes>();
+  final itemsPerPage = 20;
+  var page = 0;
+  var offset = 0;
+  var firstCall = true;
+  var lastTotalReturnedItems = 0;
+  List<Results> listH;
+  ScrollController _scrollController = new ScrollController();
   Heroes herois;
+
+  static const  baseUrl = 'https://gateway.marvel.com:443';
+  final url = baseUrl + "/v1/public/characters";
   String _textT="";
   String _aux ="";
 
@@ -41,15 +55,63 @@ class _MyHomePageState extends State<MyHomePage> {
  void initState(){
    super.initState();
    fetchData();
+   _scrollController.addListener((){
+  if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+    fetchData();
+  }
+   });
  }
 
+ @override
+void dispose(){
+  _scrollController.dispose();
+  super.dispose();
+}
 // Recebendo JSON
 fetchData() async{
-  var res = await http.get(url);
-  var  decodeJson = jsonDecode(res.body);
-  herois = Heroes.fromJson(decodeJson);
-  setState(() {});
-}
+
+
+  final ts = DateTime.now().millisecondsSinceEpoch;
+  final hash = generateMd5("$ts$privateKey$publicKey");
+
+  offset = (page * itemsPerPage);
+
+  Map<String, dynamic> queryParameters = {
+    "apikey": "22ec9cca057f5d000d1d4e535b743e61",
+    "hash": hash,
+    "ts": ts,
+    "limit": itemsPerPage.toString(),
+    "offset": offset.toString()
+  };
+
+  var res = await Dio().get(url, queryParameters: queryParameters);
+
+  setState(() {
+    var  decodeJson = jsonDecode(res.toString());
+    herois = Heroes.fromJson(decodeJson);
+      });
+
+     this.lastTotalReturnedItems = herois.data.count;
+      page++;
+
+  firstCall = false;
+  listH = herois.data.results;
+
+  if (!firstCall) {
+    if (this.lastTotalReturnedItems < itemsPerPage) {
+      listH.addAll(herois.data.results);
+    }
+  }
+  }
+
+
+  generateMd5(String data) {
+    var content = new Utf8Encoder().convert(data);
+    var md5 = crypto.md5;
+    var digest = md5.convert(content);
+    return hex.encode(digest.bytes);
+  }
+
 
 // Visual
   @override
@@ -67,17 +129,24 @@ fetchData() async{
       ),
       drawer: Drawer(),
         body: herois == null?Center(child: CircularProgressIndicator(),)
-        : GridView.count(crossAxisCount: 2, children: herois.data.results.map((hero) => Padding(
+
+        : GridView.count(crossAxisCount: 2, controller: _scrollController, children: herois.data.results.map((hero) => Padding(
           padding: const EdgeInsets.all(2.0),
+
           child: InkWell(
+
             onTap: (){
               Navigator.push(context, MaterialPageRoute(builder: (context) => HeroesDetail(
                 heroisM: hero,
+
               )));
+
             },
+
     child:Hero(
       tag: hero.thumbnail,
           child:Card(
+
             elevation: 3.0,
               child: Column(
                 mainAxisAlignment:  MainAxisAlignment.spaceEvenly,
@@ -100,14 +169,15 @@ fetchData() async{
 }
 
 class DataSearch extends SearchDelegate<String>{
-  final heroes = [
-    "Hulk",
-    "Homem de Ferro",
-    "Viuva Negra"
-  ];
   final recentHeroes = [
     "Hulk"
   ];
+
+  final heroes = [
+    "Hulk"
+
+  ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     // Botão limpar quandoe stá pesquisando
